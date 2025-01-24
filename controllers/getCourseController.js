bc19d72659c905e29682144d899e1410d4f2ba36
mubashirmi <111482469+mubashirmi@users.noreply.github.com>
2025-01-24 16:01:58 +0500

    final push

--- controllers/getCourseController.js
+++ controllers/getCourseController.js
@@ -1,40 +1,68 @@
-const { Course, Part, Video } = require("../models/index");
+const { Course, Part, Video, Assessment, Question } = require("../models/index");
 
 // For admin to get All Courses 
-exports.getAllCourses = async( req , res )=>{
-    try{
+exports.getAllCourses = async (req, res) => {
+    try {
         const courses = await Course.findAll({
-            include : {
-                model : Part ,
-                include :Video
-            }
-        })
+            include: {
+                model: Part,
+                include: [
+                    {
+                        model: Video,
+                    },
+                    {
+                        model: Assessment,
+                        include: [
+                            {
+                                model: Question,
+                                attributes: ['id', 'text', 'options'], // Fetch only necessary attributes
+                            },
+                        ],
+                    },
+                ],
+            },
+        });
 
-        if(!courses || courses.length === 0){
-            res.status(200).json({message : "No Courses Exist"});
-        }else{
-            res.status(200).json(courses);
+        if (!courses || courses.length === 0) {
+            return res.status(200).json({ message: "No Courses Exist" });
         }
-    }catch(err){
-        res.status(500).json({message:"Internal Server Error", error: err})
-    }
-}
 
-// To get information of sinngle course 
-exports.getCourseById = async( req , res )=>{
+        res.status(200).json(courses);
+    } catch (err) {
+        res.status(500).json({ message: "Internal Server Error", error: err });
+    }
+};
+
+exports.getCourseById = async (req, res) => {
     const { id } = req.params;
-    try{
-        const course = await Course.findByPk(id,{
-            include : {
-                model : Part ,
-                include :Video
-            }
-        })
-        if(!course){
-            res.status(404).json({ message : "Course Not Found" });
+
+    try {
+        const course = await Course.findByPk(id, {
+            include: {
+                model: Part,
+                include: [
+                    {
+                        model: Video,
+                    },
+                    {
+                        model: Assessment,
+                        include: [
+                            {
+                                model: Question,
+                                attributes: ['id', 'text', 'options'], // Fetch only necessary attributes
+                            },
+                        ],
+                    },
+                ],
+            },
+        });
+
+        if (!course) {
+            return res.status(404).json({ message: "Course Not Found" });
         }
-        res.status(200).json(course)
-    }catch(err){
-        res.status(500).json({message:"Internal Server Error", error: err})
+
+        res.status(200).json(course);
+    } catch (err) {
+        res.status(500).json({ message: "Internal Server Error", error: err });
     }
-}
+};
