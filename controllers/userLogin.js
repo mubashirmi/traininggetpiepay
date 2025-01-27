12719b6ae7b39dea469a84be15d4e3a82f025d22
mubashirmi <111482469+mubashirmi@users.noreply.github.com>
2025-01-27 12:49:49 +0500

    added id in login

--- controllers/userLogin.js
+++ controllers/userLogin.js
@@ -5,14 +5,13 @@ exports.userLogin = async (req,res)=>{
     try{
         const {email} = req.body;
         let user = await User.findOne({where:{email}});
-        console.log(user)
         if(user){
             const token = jwt.sign({ id: user.id, email }, process.env.JWT_SECRET, { expiresIn: '1h' }); 
-            res.status(200).json({message : "User LoggedIn Successfully",token});
+            res.status(200).json({message : "User LoggedIn Successfully",userId : user.id,token});
         }else{
             user = await User.create({ email });
             const token = jwt.sign({ id: user.id, email }, process.env.JWT_SECRET, { expiresIn: '1h' });
-            res.status(200).json({message : "User Created Successfully",token})
+            res.status(200).json({message : "User Created Successfully",userId : user.id,token})
         }
     }catch(err){
         res.status(500).json({message :"Internal Server Error", error:err.message})
